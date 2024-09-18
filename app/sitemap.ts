6e84d2e624e3982ea95885f0a6697278dca0a0f7
Wendaolee <leewendao@outlook.com>
2024-09-18 17:44:31 +0800

    Feat: 将所有博文内容向站点地图提供

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,44 @@
 import type { MetadataRoute } from 'next'
+import { rankedBlogs,rankedEssays,rankedTranslations,rankedNotes,rankedStory,rankedWritings } from '@/lib/filteredDatas'
  
 export default function sitemap(): MetadataRoute.Sitemap {
+  const blogs:MetadataRoute.Sitemap = rankedBlogs.map(ele => ({
+    url: `https://leewendao.otterstack.cn/${ele.slug}`,
+    lastModified: ele.date,
+    changeFrequency: 'weekly',
+    priority: 0.8,
+  }))
+
+  const essays:MetadataRoute.Sitemap = rankedEssays.map(ele => ({
+    url: `https://leewendao.otterstack.cn/${ele.slug}`,
+    lastModified: ele.date,
+    changeFrequency: 'weekly',
+    priority: 0.5,
+  }))
+
+  const translations:MetadataRoute.Sitemap = rankedTranslations.map(ele => ({
+    url: `https://leewendao.otterstack.cn/${ele.slug}`,
+    lastModified: ele.date,
+    changeFrequency: 'weekly',
+    priority: 0.3,
+  }))
+
+  const notes:MetadataRoute.Sitemap = rankedNotes.map(ele => ({
+    url: `https://leewendao.otterstack.cn/${ele.slug}`,
+    lastModified: ele.date,
+    changeFrequency: 'weekly',
+    priority: 0.3,
+  }))
+
+  const story:MetadataRoute.Sitemap = rankedStory.map(ele => ({
+    url: `https://leewendao.otterstack.cn/${ele.slug}`,
+    lastModified: ele.date,
+    changeFrequency: 'weekly',
+    priority: 0.3,
+  }))
+  
+  
+  
   return [
     {
       url: 'https://leewendao.otterstack.cn/',
@@ -14,5 +52,10 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'daily',
       priority: 0.8,
     },
+    ...blogs,
+    ...essays,
+    ...translations,
+    ...notes,
+    ...story,
   ]
 }
