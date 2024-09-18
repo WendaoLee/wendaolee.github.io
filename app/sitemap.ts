430f910d8cc476e4250c781834846764e1494ecf
LeeWendao <LeeWendao@outlook.com>
2024-09-18 18:14:24 +0800

    Fix： sitemap.ts 中多余的斜杠

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,35 +3,35 @@ import { rankedBlogs,rankedEssays,rankedTranslations,rankedNotes,rankedStory,ran
  
 export default function sitemap(): MetadataRoute.Sitemap {
   const blogs:MetadataRoute.Sitemap = rankedBlogs.map(ele => ({
-    url: `https://leewendao.otterstack.cn/${ele.slug}`,
+    url: `https://leewendao.otterstack.cn${ele.slug}`,
     lastModified: ele.date,
     changeFrequency: 'weekly',
     priority: 0.8,
   }))
 
   const essays:MetadataRoute.Sitemap = rankedEssays.map(ele => ({
-    url: `https://leewendao.otterstack.cn/${ele.slug}`,
+    url: `https://leewendao.otterstack.cn${ele.slug}`,
     lastModified: ele.date,
     changeFrequency: 'weekly',
     priority: 0.5,
   }))
 
   const translations:MetadataRoute.Sitemap = rankedTranslations.map(ele => ({
-    url: `https://leewendao.otterstack.cn/${ele.slug}`,
+    url: `https://leewendao.otterstack.cn${ele.slug}`,
     lastModified: ele.date,
     changeFrequency: 'weekly',
     priority: 0.3,
   }))
 
   const notes:MetadataRoute.Sitemap = rankedNotes.map(ele => ({
-    url: `https://leewendao.otterstack.cn/${ele.slug}`,
+    url: `https://leewendao.otterstack.cn${ele.slug}`,
     lastModified: ele.date,
     changeFrequency: 'weekly',
     priority: 0.3,
   }))
 
   const story:MetadataRoute.Sitemap = rankedStory.map(ele => ({
-    url: `https://leewendao.otterstack.cn/${ele.slug}`,
+    url: `https://leewendao.otterstack.cn${ele.slug}`,
     lastModified: ele.date,
     changeFrequency: 'weekly',
     priority: 0.3,
