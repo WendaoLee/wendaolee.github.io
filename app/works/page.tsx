46460ca6363d55f904caf097dda02363ff45ed11
Wendaolee <leewendao@outlook.com>
2025-01-19 16:29:08 +0800

    Feat: 添加 /works 页面

---
+++ app/works/page.tsx
@@ -0,0 +1,21 @@
+import { Metadata } from "next";
+
+export async function generateMetadata():Promise<Metadata>{
+    return {
+        title: "文章外的产出 - 李问道的博客 / Works of Wendaolee",
+    }
+}
+
+export default function WorksPage(){
+    return <div className="mt-5">
+        <header>
+            <h1 className="mb-0">文章外的产出 😼</h1>
+            <h4 className="mt-0">Slides & Publications</h4>
+            <p>此处存放的是一些可能能够帮助你的产出。</p>
+        </header>
+        <div>
+            <h3>Slides</h3>
+            <p className="mt-0">一些 PPT。</p>
+        </div>
+    </div>
+}
